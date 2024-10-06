830a22e1f72f968398e4f35ed0ad518a4fb2b7c9
AngelOttoni <angel.m.ottoni@gmail.com>
2024-10-06 00:29:42 -0300

    chore: remove unused Flask application initialization code

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,3 @@ def create_app():
     app.register_blueprint(main)
 
     return app
-
-# from flask import Flask
-# from .routes import main
-
-# def create_app():
-#     app = Flask(__name__, template_folder='templates')
-    
-#     # Registre o blueprint
-#     app.register_blueprint(main)
-    
-#     return app
