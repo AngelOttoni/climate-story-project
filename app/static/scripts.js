a82b333ecf06802b37a6e2c6ff78db6046e8cb70
AngelOttoni <angel.m.ottoni@gmail.com>
2024-10-06 00:32:21 -0300

    feat: implement fetch request to retrieve data from the API endpoint

---
+++ app/static/scripts.js
@@ -0,0 +1,8 @@
+fetch('http://127.0.0.1:5000/api/co2data')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
